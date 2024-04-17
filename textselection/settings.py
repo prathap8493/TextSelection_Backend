8a14d79850737bb54c3982352117a23789aaf409
prathap <prathap@goom.ai>
2024-04-17 23:18:10 +0530

    requirements changes

--- textselection/settings.py
+++ textselection/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'textselection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'textselection',
+        'USER': 'textselection_user',
+        'PASSWORD': 'OgExmunDaHUHHbQJzcHkVpAijjIk9Knu',
+        'HOST': 'dpg-cog0f7779t8c73cd0bi0-a',  
+        'PORT': '5432'
     }
 }
 
